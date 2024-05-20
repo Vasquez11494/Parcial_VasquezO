d361e36825638566b54af4cba168324dcbd97546
Vasquez <cvasquez857@gmail.com>
2024-05-20 08:50:48 -0600

    creacion de navbar

---
+++ vistas/templates/navbar.php
@@ -0,0 +1,11 @@
+<ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Ingresar Visitantes</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Buscar Visitantes</a>
+  </li>
+</ul>
