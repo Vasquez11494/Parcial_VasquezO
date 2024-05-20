dec727af6e42ec7414dc0c206ae7e376819a5ed8
Vasquez <cvasquez857@gmail.com>
2024-05-20 09:04:31 -0600

    se creo un navbar asi mismo, se creo index.php

--- vistas/templates/navbar.php
+++ vistas/templates/navbar.php
@@ -1,11 +1,11 @@
-<ul class="nav justify-content-center">
+<ul class="nav justify-content-center bg-info shadow">
   <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+    <a class="nav-link active text-black fs-7" aria-current="page" href="#"> <i class="bi bi-house-door-fill  me-2"></i>Inicio</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Ingresar Visitantes</a>
+    <a class="nav-link text-black fs-7" href="#"><i class="bi bi-person-plus-fill me-2"></i>Ingresar Visitantes</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link" href="#">Buscar Visitantes</a>
+    <a class="nav-link text-black fs-7" href="#"><i class="bi bi-search me-2"></i>Buscar Visitantes</a>
   </li>
 </ul>
